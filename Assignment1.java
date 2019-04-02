2269494675bb1a645fdc50a4ef2e0bedbdfee4c9
Satvik Sabharwal <satviksabharwal7@gmail.com>
2019-04-03 03:19:38 +0530

    Add files via upload
    Assignment1: Program That Accepts two strings as command line arguments and print message.
    Assignment2: Program Accept a string as a command line and print welcome message.
    Assignment3: Program to Accept two integers as a command line and print their sum.

---
+++ Assignment1.java
@@ -0,0 +1,6 @@
+public class Assignment1{
+public static void main(String[] args){
+	
+	System.out.print(args[0]+" Technologies "+args[1]);
+}
+} 
---
+++ Assignment3.java
@@ -0,0 +1,7 @@
+public class Assignment3{
+	public static void main(String[] args){
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.print("The sum of "+a+" and "+b+" is "+ (a+b));
+	}
+}
